875bc9a66cc977dad6fa743160086d9dd0b7e0d2
muhammad akbar syahwana <muhammad akbar syahwana>
2018-09-27 10:53:50 +0700

    view engine

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const app = express()
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+app.set('view engine', 'hbs')
   
 app.listen(3000, () => console.log("Its Running on port 3000"))
